36efc84616aebe3508000f2ed29d88ac1faf570f
Keva <keva@rambler.ru>
2025-08-11 11:39:03 +0300

    field configuration used in contents processing

--- context/x-contents.hpp
+++ context/x-contents.hpp
@@ -2,19 +2,20 @@
 # define __DelphiX_context_x_contents_hpp__
 # include "../contents.hpp"
 # include "text-image.hpp"
+# include "fields-man.hpp"
 
 namespace DelphiX {
 namespace context {
 
   auto  GetMiniContents(
     const Slice<const Slice<const Lexeme>>&,
-    const Slice<const textAPI::MarkupTag>& ) -> mtc::api<IContents>;
+    const Slice<const textAPI::MarkupTag>&, FieldHandler& ) -> mtc::api<IContents>;
   auto  GetBM25Contents(
     const Slice<const Slice<const Lexeme>>&,
-    const Slice<const textAPI::MarkupTag>& ) -> mtc::api<IContents>;
+    const Slice<const textAPI::MarkupTag>&, FieldHandler& ) -> mtc::api<IContents>;
   auto  GetRichContents(
     const Slice<const Slice<const Lexeme>>&,
-    const Slice<const textAPI::MarkupTag>& ) -> mtc::api<IContents>;
+    const Slice<const textAPI::MarkupTag>&, FieldHandler& ) -> mtc::api<IContents>;
 
 }}
 
