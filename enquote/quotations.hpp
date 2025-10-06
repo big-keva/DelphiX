5ad7f11a5a390e1e634b4947388568b8fa8c3850
Keva <keva@rambler.ru>
2025-10-06 17:45:18 +0300

    quotation factory

--- enquote/quotations.hpp
+++ enquote/quotations.hpp
@@ -2,16 +2,40 @@
 # define __DelphiX_enquote_quotations_hpp__
 # include "../text-api.hpp"
 # include "../slices.hpp"
+# include "../fields.hpp"
+# include "../queries.hpp"
 # include <functional>
 
 namespace DelphiX {
 namespace enquote {
 
-  using QuotesFunction = std::function<void(textAPI::IText*, const Slice<const char>&)>;
+  using QuotesFunc = std::function<void(
+    textAPI::IText*,
+    const Slice<const char>&,
+    const Slice<const char>&,
+    const queries::Abstract&)>;
 
-  void  AsListOfQuotes( textAPI::IText*, const Slice<const char>& );
-  void  AsMarkedQuotes( textAPI::IText*, const Slice<const char>& );
-  void  AsOriginalText( textAPI::IText*, const Slice<const char>& );
+  class QuoteMachine
+  {
+    class common_settings;
+    class quoter_function;
+
+  public:
+    QuoteMachine( const FieldHandler& );
+    QuoteMachine( const QuoteMachine& ) = default;
+
+  public:
+    auto  SetLabels( const char* open, const char* close ) -> QuoteMachine&;
+    auto  SetIndent( const FieldOptions::indentation& ) -> QuoteMachine&;
+
+  public:
+    auto  Structured() -> QuotesFunc;
+    auto  TextSource() -> QuotesFunc;
+
+  protected:
+    std::shared_ptr<common_settings>  settings;
+
+  };
 
 }}
 
