b03681452306a470f153d634ede6a75734dba4ec
Keva <keva@rambler.ru>
2025-10-09 18:19:49 +0300

    decompress in limits

--- src/queries/decompressor.hpp
+++ src/queries/decompressor.hpp
@@ -11,6 +11,36 @@ namespace queries {
   struct LoadForm
     {  const char* operator()( const char* src, uint8_t& fid ) const {  return ::FetchFrom( src, fid );  }  };
 
+  template <class GetFormId, class RankEntry>
+  auto  UnpackEntries(
+    Abstract::EntrySet* output,
+    Abstract::EntryPos* appPtr,
+    unsigned            maxLen, const StrView& source, const RankEntry& ranker, unsigned id ) -> unsigned
+  {
+    auto  srcPtr( source.data() );
+    auto  srcEnd( source.data() + source.size() );
+    auto  outPtr = output;
+    auto  outEnd = outPtr + maxLen;
+    auto  uEntry = unsigned(0);
+
+    while ( srcPtr < srcEnd && outPtr != outEnd )
+    {
+      uint8_t   formid;
+      unsigned  uOrder;
+      double    weight;
+
+      srcPtr = GetFormId()( ::FetchFrom( srcPtr, uOrder ), formid );
+
+      if ( (weight = ranker( uEntry = (uOrder += uEntry), formid )) < 0 )
+        continue;
+
+      *outPtr++ = { { uOrder, uOrder }, weight, double(uOrder), { appPtr, appPtr + 1 } };
+      *appPtr++ = { id, uEntry++ };
+    }
+
+    return unsigned(outPtr - output);
+  }
+
   template <class GetFormId, size_t N, size_t M, class RankEntry>
   auto  UnpackEntries(
     Abstract::EntrySet (&output)[N],
